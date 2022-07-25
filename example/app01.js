cf6d2e5706d7c260b0d7243a2cc5b9b4a62fd079
Jo hyera <s2037@e-mirim.hs.kr>
2022-07-25 14:50:08 +0900

    module 예제 : 절댓값 원의 면적 구하기

--- example/app01.js
+++ example/app01.js
@@ -1,2 +1,3 @@
 var result = require('./module01')
 console.log(result.circleArea(20))
+console.log(result.abs(-1))
--- example/module01.js
+++ example/module01.js
@@ -9,3 +9,12 @@ console.log(result.circleArea(10))*/
 exports.circleArea = function(num){
     return num * num * Math.PI;
 }
+
+// 절댓값을 구한다.
+exports.abs = function(num){
+    if(num > 0)
+    return num;
+    else return -num;
+}
+
+module.exports = {abs, circleArea};
