89b7160d871c7429a1c73f1ca047c4dfbc2246d9
Jo hyera <s2037@e-mirim.hs.kr>
2022-07-25 11:14:56 +0900

    문자열 합칠 때

---
+++ app08.js
@@ -0,0 +1,14 @@
+// 과거
+/*var num1 = 1
+var num2 = 2
+var result = 3
+var string = num1 + `더하기` + num2 + '는 ' + result
+console.log(string)
+*/
+
+// 현재
+const num1 = 1
+const num2 = 2
+const result = 3
+const string = `${num1} 더하기 ${num2} 는 ${result}`
+console.log(string)
