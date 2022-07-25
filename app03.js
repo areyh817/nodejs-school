096915c21ce073d0a2b282fe7ac6aeaf77d31259
Jo hyera <s2037@e-mirim.hs.kr>
2022-07-25 10:37:34 +0900

    문자색 설정

---
+++ app03.js
@@ -0,0 +1,9 @@
+console.log("\u001b[31m", 'Hello World....');
+console.log("\u001b[32m", 'Hello World....');
+console.log("\u001b[33m", 'Hello World....');
+console.log("\u001b[34m", 'Hello World....');
+console.log("\u001b[35m", 'Hello World....');
+console.log("\u001b[36m", 'Hello World....');
+console.log("\u001b[37m", 'Hello World....');
+console.log("\u001b[0m");
+console.log('Hello World....');
