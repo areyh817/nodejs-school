d4da79c44192da5f6db2614d77b1c5533807d5f0
Jo hyera <s2037@e-mirim.hs.kr>
2022-07-26 09:53:07 +0900

    비동기식 파일 읽기

---
+++ basic/app19.js
@@ -0,0 +1,27 @@
+const fs = require('fs'); // 비동기식 파일 읽기
+console.log('시작');
+fs.readFile('./sam.txt', (err, data) => {
+    if(err){
+        throw err;
+    }
+
+    console.log('1번', data.toString());
+});
+
+fs.readFile('./sam.txt', (err, data) => {
+    if(err){
+        throw err;
+    }
+
+    console.log('2번', data.toString());
+});
+
+fs.readFile('./sam.txt', (err, data) => {
+    if(err){
+        throw err;
+    }
+
+    console.log('3번', data.toString())
+});
+
+console.log('끝')
