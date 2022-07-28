6e17cd2f07398b008a2dd55080395933a55118eb
Jo hyera <s2037@e-mirim.hs.kr>
2022-07-28 10:31:20 +0900

    미들웨어 사용

--- express/express03.js
+++ express/express03.js
@@ -7,17 +7,25 @@ app.set('port', process.env.port || 4444);
 // 미들웨어 사용
 app.use(function(req, res, next) {
     console.log('첫 번째 미들웨어\n');
+    // 다른 곳으로 사이트 이동
+    res.redirect('http://www.gogle.com')
     req.user = 'areyh';
     req.next();
     /*res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
     res.end('<h1>서버에서 응답한 결과 : ' + req.user + '</h1>')*/
 });
 
-// 두 번째 미들웨어 사용
+// 두 번째 미들웨어 사용 _json 형태로 정의해서 send()로 가져온다.
 app.use(function(req, res, next){
-    console.log('두 번째 미들웨어')
-    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'});
-    res.end('<h1>서버에서 응답한 결과 : ' + req.user + '</h1>')
+    console.log('두 번째 미들웨어_send()')
+    // json 형태로 정의
+    var student = {name : '홍길동', tel : '010-123-4568'};
+    res.send(student);
+    var studentStr = JSON.stringify(student)
+    // res.send(studentStr);
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+    res.end(studentStr);
+
 });
 
 var server = http.createServer(app).listen(app.get('port'), function(){
